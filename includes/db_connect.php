933a2b5e518291fe0db63b96d6cf7156bd09c3ce
alizhj <lisahjarpe@gmail.com>
2018-06-13 20:25:13 +0200

    extra bets hidden

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,13 +1,10 @@
 <?php
-	define("DB_HOST", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "root");
-	define("DB_NAME", "fifa2018");
 
-	// define("DB_HOST", "us-cdbr-iron-east-04.cleardb.net");
-	// define("DB_USER", "bdb8f6a6942bdc");
-	// define("DB_PASS", "fd97bab6");
-	// define("DB_NAME", "heroku_026fda314241625");
+
+	define("DB_HOST", "us-cdbr-iron-east-04.cleardb.net");
+	define("DB_USER", "bdb8f6a6942bdc");
+	define("DB_PASS", "fd97bab6");
+	define("DB_NAME", "heroku_026fda314241625");
 
 	$db_connect = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
